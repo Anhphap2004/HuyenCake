9eb360474b18ef66c7bb7ab33daeae68def181ce
Anhphap2004 <phapmeo2k4@gmail.com>
2025-04-10 14:40:34 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HuyenCake.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
