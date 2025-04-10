bfcad1a9b4e61043ce1ae53cf0e6cdf4c6026fcd
Anhphap2004 <phapmeo2k4@gmail.com>
2025-04-10 15:21:32 +0700

    Hiển Thị Phần Giới Thiệu Web

---
+++ ViewComponents/AboutViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using HuyenCake.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace HuyenCake.ViewComponents
+{
+    public class AboutViewComponent : ViewComponent
+    {
+        private readonly HuyenCakeContext _context;
+        public AboutViewComponent(HuyenCakeContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var about = await _context.Abouts.ToListAsync();
+            return View(about);
+        }
+    }
+}
